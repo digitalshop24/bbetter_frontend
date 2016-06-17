ae5308945c60e88b626d4b7338b34291fbe30a23
Artsiom Stsepanishchau <artemjackson95@gmail.com>
2016-06-17 13:28:48 +0300

    Injected contact trainer functionality into app

--- src/bbetter/profile/index.js
+++ src/bbetter/profile/index.js
@@ -1,8 +1,11 @@
 import angular from 'angular';
 import template from './template';
+import contactTrainer from './contact-trainer';
 import style from './style';  //eslint-disable-line
 
-export default angular.module('bbetter.profile', [])
+export default angular.module('bbetter.profile', [
+  contactTrainer.name
+])
   .config($stateProvider => {
     'ngInject';
     $stateProvider.state('bbetter.profile', {

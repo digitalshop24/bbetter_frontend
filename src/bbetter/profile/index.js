9e8da0f851f0b2535d494e2af841bb2e4de900df
Artsiom Stsepanishchau <artemjackson95@gmail.com>
2016-06-17 16:13:01 +0300

    Injected calories calculator into app

--- src/bbetter/profile/index.js
+++ src/bbetter/profile/index.js
@@ -1,8 +1,11 @@
 import angular from 'angular';
 import template from './template';
+import calorieCalculator from './calorie-calculator';
 import style from './style';  //eslint-disable-line
 
-export default angular.module('bbetter.profile', [])
+export default angular.module('bbetter.profile', [
+  calorieCalculator.name
+])
   .config($stateProvider => {
     'ngInject';
     $stateProvider.state('bbetter.profile', {

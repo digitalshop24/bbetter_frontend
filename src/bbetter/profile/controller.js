4b41de839c08147501a9f25735c143bc71d8621f
Artsiom Stsepanishchau <artemjackson95@gmail.com>
2016-06-20 12:17:35 +0300

    Added video reports retrieving

---
+++ src/bbetter/profile/controller.js
@@ -0,0 +1,7 @@
+export default class ProfileCtrl {
+  constructor(instructions, reports) {
+    "ngInject";
+    this.instructions = instructions;
+    this.reports = reports;
+  }
+}
--- src/bbetter/profile/index.js
+++ src/bbetter/profile/index.js
@@ -1,11 +1,27 @@
 import angular from 'angular';
+import controller from './controller';
 import template from './template';
+import videos from './videos';
 import style from './style';  //eslint-disable-line
 
-export default angular.module('bbetter.profile', [])
+export default angular.module('bbetter.profile', [
+  videos.name
+])
   .config($stateProvider => {
     'ngInject';
     $stateProvider.state('bbetter.profile', {
+      controller,
+      controllerAs: 'ctrl',
+      resolve: {
+        /* Hardcoded instructions */
+        instructions: () => [{}, {}, {}, {}, {}, {}],
+        reports: (api, user) => {
+          "ngInject";
+          const {token: authToken} = user;
+
+          return api.getV1Videos({authToken});
+        }
+      },
       template,
       url: '/profile'
     });

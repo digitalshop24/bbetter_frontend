64b58d58f0aeabdef879bdfd0dc598f54262617e
Andrey Fedorovich <a.d.fedorovich@gmail.com>
2016-06-20 17:06:41 +0300

    rebuild footer / mobile carousel / make up for mobile (LANDING)

--- src/bbetter/landing/controller.js
+++ src/bbetter/landing/controller.js
@@ -1,6 +1,15 @@
 'use strict';
 export default class LandCtrl {
     constructor() {
+        this.responsCarousel = [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                dots:true
+              }
+            }
+        ];
         this.active_slide_1 = 'active';
         this.active_slide_2 = '';
         this.active_slide_3 = '';

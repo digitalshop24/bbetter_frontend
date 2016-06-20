c4ac8e7a911dc0238e638993e5e1c5b6e3a2c9dd
Andrey Fedorovich <a.d.fedorovich@gmail.com>
2016-06-20 14:04:44 +0300

    mob styles version2

---
+++ src/bbetter/profile/videos/controller.js
@@ -0,0 +1,60 @@
+export default class VideosCtrl {
+  constructor(api, user) {
+    "ngInject";
+    this.api = api;
+    this.user = user;
+
+    this.reports = this.updateReportsToBeLengthOf6(this.reports);
+    console.log(this.reports);
+
+    this.selectedInstruction = this.instructions[0];
+    this.selectedReport = this.reports[0];
+
+    this.instructionsDisplayed = true;
+  }
+
+  displayInstructions() {
+    this.instructionsDisplayed = true;
+  }
+
+  displayReports() {
+    this.instructionsDisplayed = false;
+  }
+
+  selectInstruction(instruction) {
+    this.selectedInstruction = instruction;
+  }
+
+  selectReport(report) {
+    this.selectedReport = report;
+  }
+
+  updateReportsToBeLengthOf6(reports) {
+    const updated = [];
+
+    for (let i = 1; i <= 6; ++i) {
+      const element = reports.find(e => e.day === i);
+      if (element) {
+        updated.push(element);
+      } else {
+        updated.push({day: i});
+      }
+    }
+
+    return updated;
+  }
+
+  uploadVideo(youtubeUrl) {
+    const {token: authToken} = this.user;
+
+    return this.api.postV1Videos({authToken, youtube_code: youtubeUrl})
+      .then(report => {
+        const {day} = report;
+
+        this.reports[day - 1] = report;
+
+        this.selectReport(report);
+      })
+      .catch(response => console.error(response));
+  }
+}

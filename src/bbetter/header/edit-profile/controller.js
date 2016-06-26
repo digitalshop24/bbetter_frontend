9aecf796d24474e45a8ee801260f4abd75cc0014
Ivan <kotato@users.noreply.github.com>
2016-06-26 17:05:52 +0300

    Add promocodes to registration and edit forms

--- src/bbetter/header/edit-profile/controller.js
+++ src/bbetter/header/edit-profile/controller.js
@@ -1,19 +1,24 @@
 export default class RegistartionCtrl {
-  constructor(api, user, $state, $uibModalInstance) {
+  constructor(api, user, $state, $uibModalInstance, $scope) {
     "ngInject";
     this.api = api;
     this.user = user;
     this.state = $state;
+    this.scope = $scope;
     this.instance = $uibModalInstance;
   }
 
   edit(user) {
+    this.scope.error = null;
     const {token: authToken} = this.user;
 
     return this.api.putV1UsersEdit({authToken, ...user})
       .then(profile => Object.assign(this.user, profile))
       .then(() => this.instance.close())
-      .catch(response => console.error(response));
+      .catch(response => {
+        console.error(response);
+        this.scope.error = response.body.message;
+      } );
   }
 
   close() {
